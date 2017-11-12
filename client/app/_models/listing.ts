923b90cb0cc304992a55f9e53312c5ab688781d3
diarmuidn4 <diarmuidn4@gmail.com>
2017-11-12 13:48:44 +0000

    Commit all changes

--- client/app/_models/listing.ts
+++ client/app/_models/listing.ts
@@ -1,16 +1,8 @@
-//added necessary extra variables
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-var ObjectId = Schema.ObjectId;
-var User = mongoose.model('User');
-
-var ListingSchema = new Schema( {
-    _id : ObjectId,
-    userId: { type: Schema.ObjectId, ref: "User" },
-    address: { type: String, required: true },
-    description: { type: String, required: true },
-    rooms: { type: Number, required: true },
-    deposit: { type: Number, required: true },
-});
-
-var Listing = mongoose.model('Listing', ListingSchema);
+export class Listing {
+    _id : String;
+    userId: String;
+    address: String;
+    description: String;
+    rooms: Number ;
+    deposit: Number;
+}
--- client/app/_models/user.ts
+++ client/app/_models/user.ts
@@ -1,16 +1,10 @@
-﻿//added necessary extra variables
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-var ObjectId = Schema.ObjectId
+﻿export class User {
+    _id : string;
+    username:  String;
+    password: String;
+    firstName: String;
+    lastName:String;
+    email: String;
+    userType: String;
+}
 
-var UserSchema = new Schema( {
-    _id : ObjectId,
-    username: { type: String, required: true },
-    password: { type: String, required: true },
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    email: { type: String, required: true },
-    userType: { type: String, required: true },
-});
-
-var User = mongoose.model('User', UserSchema);
